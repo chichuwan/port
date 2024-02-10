13270b10dabbf3336d1d5f606fa6b77888b8bb10
chichuwan <112555157+chichuwan@users.noreply.github.com>
2024-02-10 22:22:27 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var typeingEffect = new Typed(".multiText", {
+    strings : ["Chichuuu", "a Web Developer","a CS Student"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 1500
+})
+
+
+document.querySelector("#ccv").addEventListener("click", function(){
+    window.open("https://chichuwan.github.io/cardValidation/", "_blank");
+})
+
+document.querySelector("#twinstax").addEventListener("click", function(){
+    window.open("https://twin-stax.com/", "_blank");
+})
+
+document.querySelector("#pengu").addEventListener("click", function(){
+    window.open("https://pengutoken.io/", "_blank");
+})
